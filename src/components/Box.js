c3e0626b0b6ce6622255e6f1b13d7aa48ce0bbb0
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-11-03 19:35:40 +0400

    Create box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -15,6 +15,15 @@ class Box extends Component {
     return (
       <div>
         <h1>Box Maker</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="boxWidth">Box Width:</label>
+          <input type="number" id="boxWidth" name="boxWidth" />
+          <label htmlFor="boxHeight">Box Height:</label>
+          <input type="number" id="boxHeight" name="boxHeight" />
+          <label htmlFor="boxColor">Box Color:</label>
+          <input type="color" id="boxColor" name="boxColor" />
+          <button type="submit">Add New Box</button>
+        </form>
       </div>
     );
   }
